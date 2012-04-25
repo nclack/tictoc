e40d05442a9908b619427be8fce2fb5dd27ad24b
Nathan Clack <clackn@janelia.hhmi.org>
2012-04-25 09:58:45 -0400

    [docs] changed some comments to use doxygen style.

--- src/tictoc.h
+++ src/tictoc.h
@@ -8,13 +8,12 @@ extern "C"{
 #include <stdint.h>
 
 typedef struct _tic_toc_timer
-{ uint64_t last; // last observation on the counter
-  uint64_t rate; // counts per second  
+{ uint64_t last; ///< last observation on the counter
+  uint64_t rate; ///< counts per second  
 } TicTocTimer;
 
-TicTocTimer tic(void);
-double      toc(TicTocTimer *last); // returns time since last in seconds and updates timer 
-double      toc(TicTocTimer *last); // returns time since last in seconds and updates timer
+TicTocTimer tic(void);              ///< Intitializes a timer.
+double      toc(TicTocTimer *last); ///< Updates timer. \param[in,out] if \a last is NULL, a global timer is used. \returns time since last in seconds.
 
 
 #ifdef __cplusplus
